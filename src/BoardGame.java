453997ed2c4352524319730ab118b9d39ad8ff11
UH60-BlackHawk <acai27@nobles.edu>
2025-02-20 18:00:51 +0000

    nahman and alex

--- src/BoardGame.java
+++ src/BoardGame.java
@@ -1,21 +1,24 @@
 package src;
+
 public class BoardGame {
     private String[][] board;
     private int playerRow;
     private int playerCol;
+    private int counter;
     
     public BoardGame(int rows, int cols) {
         board = new String[rows][cols];
         // Initialize the board with empty spaces
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                board[i][j] = "-";
+                board[i][j] = "O";
             }
         }
         // Place player at starting position (0,0)
         playerRow = 0;
         playerCol = 0;
         board[playerRow][playerCol] = "P";
+        counter = 0;
     }
     
     public void displayBoard() {
@@ -29,11 +32,28 @@ public class BoardGame {
     
     // Students will implement these methods
     public void movePlayer(String direction) {
-        // TODO: Implement player movement (up, down, left, right)
+        String directionL = direction.toLowerCase();
+        if(directionL.equals("w")){
+            playerRow --;
+        }else if(direction.equals("s")){
+            playerRow ++;
+        }else if(direction.equals("d")){
+            playerCol ++;
+        }else if(direction.equals("a")){
+            playerCol --;
+        }else{
+            System.out.println("ERROR, ENTER EITHER W, A, S, OR D");
+        }
+        counter ++;
     }
     
-    public boolean isGameOver() {
-        // TODO: Implement win/lose conditions
+    public boolean isGameLost() {
+        if (playerRow == 5 || playerRow == -1 ||playerCol == 5 || playerCol == -1 ){
+            return true;
+        }
+        if (counter == 10){
+            return true;
+        }
         return false;
     }
 } 
--- src/GameRunner.java
+++ src/GameRunner.java
@@ -6,10 +6,10 @@ public class GameRunner {
         BoardGame game = new BoardGame(5, 5);
         Scanner scanner = new Scanner(System.in);
         
-        System.out.println("Welcome to the Board Game!");
+        System.out.println("Welcome to the Board Game! Try to find the Hidden Alan Cai!");
         System.out.println("Use W(up), S(down), A(left), D(right) to move");
         
-        while (!game.isGameOver()) {
+        while (!game.isGameLost()) {
             game.displayBoard();
             System.out.print("Enter your move: ");
             String move = scanner.nextLine().toUpperCase();
